fe8f2f004fbee59a38b5a46418565c5b83a554ad
Fakhraddin <faseyfra@gmail.com>
2024-08-27 17:23:57 +0200

    route get for catalog and products

--- routes/products.js
+++ routes/products.js
@@ -6,6 +6,27 @@ const Product = require('../models/products');
 const { checkBody } = require('../modules/checkBody');
 // const authenticateToken = require('../middleware/authMiddleware');
 
+// GET all products
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json(products); // Sends all products as a JSON response
+  } catch (error) {
+    console.error("Failed to retrieve products: ", error);
+    res.status(500).json({ message: "Failed to retrieve products", error: error.message });
+  }
+});
+
+router.get('/categories', async (req, res) => {
+  try {
+      const categories = await Product.distinct("category");
+      res.json(categories);
+  } catch (error) {
+      console.error("Error fetching categories:", error);
+      res.status(500).json({ message: "Failed to fetch categories", error: error.message });
+  }
+});
+
 
 /* GET product info */
 router.get('/product-info/:slug', function(req, res, next) {
@@ -63,5 +84,8 @@ router.post('/product-add/', function(req, res, next) {
   });
 
 });
+
+
+
 
 module.exports = router;
