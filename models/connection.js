fc1f69cb478104c4b8c9aa945df5d1ee92b57081
Duc Son Nguyen <duc.son.nguyen48@gmail.com>
2024-08-30 19:42:37 +0200

    bug fixed?

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.CONNECTION_STRING;
+const connectionString = `process.env.CONNECTION_STRING`;
 
 mongoose.set('strictQuery', false)
 
